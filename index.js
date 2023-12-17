9da57f542066d416422b6a1cb19995cb3ecb6d53
AKnasir <152478375+AKnasir@users.noreply.github.com>
2023-12-17 03:05:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Challenge 1: Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// Challenge 2: Random
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Challenge 3: Modulo
+let num3 = 40;
+let num4 = 6;
+let mod = num3 % num4;
+
+// Challenge 4: Max
+let max = Math.max(1, 3, 5, 7, 20);
+
+// Testing the variables
+console.log(multiply); // Output: 62
+console.log(random);   // Output: Random integer greater than 0
+console.log(mod);      // Output: 4
+console.log(max);      // Output: 20
